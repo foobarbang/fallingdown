91d41e6c7d4ae2536abc6b3404932ea75f593318
foobarbang <jarad.william.medbery@gmail.com>
2018-11-29 16:28:11 -0800

    Update script.js

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ var jcontent = {
   "name": "sword",
   "price": "10"
 }
-var output = document.getElementByID('output')
+var output = document.getElementById('output')
 output.innerHTML = jcontent.name + ' ' + jcontent.price;
