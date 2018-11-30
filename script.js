867cafd4d68515ffc7b24934d3171f939b4663c9
foobarbang <jarad.william.medbery@gmail.com>
2018-11-29 17:21:03 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 var jcontent = {
-  "name": "sword",
-  "price": "10"
+    "Items": {
+        "Weapon": [
+            {"name":"Sentra", "type":"axe", "weight":10},
+            {"name":"Maxima", "type":"sword", "weight":10},
+            {"name":"Skyline", "type":"dagger", "weight":10}
+        ],
+        "Armor": [
+            {"name":"Taurus", "type":"plate", "weight":100},
+            {"name":"Escort", "type":"chain", "weight":100}
+        ]
+    }
 }
 var output = document.getElementById('output');
 output.innerHTML = jcontent.name + ' ' + jcontent.price;
